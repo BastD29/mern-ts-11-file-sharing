40308ef80f342b4cbb0308c68e4fb907824a46ed
BastD29 <bastiendaniel9@gmail.com>
2024-07-31 17:47:22 +0200

    download file controller

--- server/src/controllers/file.ts
+++ server/src/controllers/file.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import File from "../models/file";
+import path from "path";
 
 const uploadFile = async (req: Request, res: Response) => {
   try {
@@ -18,4 +19,31 @@ const uploadFile = async (req: Request, res: Response) => {
   }
 };
 
-export { uploadFile };
+const downloadFile = async (req: Request, res: Response) => {
+  try {
+    console.log("Downloaded file details:", req.file);
+    const filename = req.file?.filename;
+    console.log("filename:", filename);
+    const file = await File.findOne({ filename });
+    console.log("file:", file);
+
+    if (!file) {
+      return res.status(404).send({ message: "File not found" });
+    }
+
+    const filePath = path.resolve(file.path);
+    console.log("filePath:", filePath);
+
+    res.download(filePath, file.filename, (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send({ message: "Error downloading file" });
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Error downloading file" });
+  }
+};
+
+export { uploadFile, downloadFile };
