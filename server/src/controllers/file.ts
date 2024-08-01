b329c7c211c410cd2d0297700573fb07a0372fbe
BastD29 <bastiendaniel9@gmail.com>
2024-08-01 12:02:40 +0200

    serve uploads folder static to access it dynamically

--- server/src/controllers/file.ts
+++ server/src/controllers/file.ts
@@ -1,17 +1,24 @@
 import { Request, Response } from "express";
 import File from "../models/file";
-import path from "path";
+// import path from "path";
 
 const uploadFile = async (req: Request, res: Response) => {
   try {
     console.log("Uploaded file details:", req.file);
+
+    const fileUrl = `${req.protocol}://${req.get("host")}/uploads/${
+      req.file?.filename
+    }`;
+    console.log("fileUrl:", fileUrl);
+
     const newFile = new File({
       filename: req.file?.originalname,
-      path: req.file?.path,
+      // path: req.file?.path,
+      path: fileUrl,
     });
 
     await newFile.save();
-    // res.json(newFile);
+
     res.status(200).send({ message: "File uploaded successfully", newFile });
   } catch (error) {
     console.error(error);
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import { NODE_ENV, PORT } from "./config/environments";
 import { connectDB } from "./config/db";
 import { corsOptions } from "./config/cors";
 import file from "./routes/file";
+import path from "path";
 
 connectDB();
 
@@ -12,8 +13,10 @@ const app = express();
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/api/files", file);
+const uploadsPath = path.join(__dirname, "../uploads");
+console.log("uploadsPath:", uploadsPath);
+app.use("/uploads", express.static(uploadsPath));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}, ${NODE_ENV} environment`);
